79765cb19dbe732c9dc33a2f3d7e712d4b2b636d
Alexander Lex <alexander.lex@gmail.com>
2019-10-01 12:04:14 -0600

    cleanup

--- lectures/lecture-advanced-js/custom_promises.js
+++ lectures/lecture-advanced-js/custom_promises.js
@@ -1,40 +1,40 @@
 const status = true;
 
 function statusCheckWithCallbacks(ifStatusPassed, ifStatusFailed) {
-  if (status) {
-    ifStatusPassed("Status has passed");
-  } else {
-    ifStatusFailed("Status has failed");
-  }
+    if (status) {
+        ifStatusPassed("Status has passed");
+    } else {
+        ifStatusFailed("Status has failed");
+    }
 }
 
 statusCheckWithCallbacks(
-  passMessage => {
-    console.log(passMessage);
-    console.log("We can now render our visualization!");
-  },
-  failMessage => {
-    console.error(failMessage);
-    console.log("Please handle the error!");
-  }
+    passMessage => {
+        console.log(passMessage);
+        console.log("We can now render our visualization!");
+    },
+    failMessage => {
+        console.error(failMessage);
+        console.log("Please handle the error!");
+    }
 );
 
 function statusCheckWithPromise() {
-  return new Promise((resolve, reject) => {
-    if (status) {
-      resolve("Status has passed");
-    } else {
-      reject("Status has failed");
-    }
-  });
+    return new Promise((resolve, reject) => {
+        if (status) {
+            resolve("Status has passed");
+        } else {
+            reject("Status has failed");
+        }
+    });
 }
 
 statusCheckWithPromise()
-  .then(passMessage => {
-    console.log(passMessage);
-    console.log("We can now render our visualization!");
-  })
-  .catch(failMessage => {
-    console.error(failMessage);
-    console.log("Please handle the error!");
-  });
+    .then(passMessage => {
+        console.log(passMessage);
+        console.log("We can now render our visualization!");
+    })
+    .catch(failMessage => {
+        console.error(failMessage);
+        console.log("Please handle the error!");
+    });
